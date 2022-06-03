957c10666d6c1dfef76fe85ed686db79d6acdcc7
nagamorikoshi <nagamori329@gmail.com>
2022-06-03 06:21:40 +0000

    adminコントローラー作成

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,2 +1,33 @@
 class Admin::GenresController < ApplicationController
+  before_action :authenticate_admin!
+  before_action :ensure_genre, onli: [:show, :edit, :update]
+
+  def index
+    @genre = Genre.new
+    @genres = Genre.all
+  end
+
+  def create
+    @genre = Genre.new(genre_params)
+    if @genre.save
+      redirect_to admin_genres_path
+    else
+      @genres = Genre.all
+      render :index
+    end
+  end
+
+  def edit
+  end
+
+  private
+
+  def genre_params
+    params.require(:genre).permit(:name, is_active)
+  end
+
+  def ensure_genre
+    @genre = Genre.find(params[:id])
+  end
+
 end
