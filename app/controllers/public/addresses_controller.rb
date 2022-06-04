f23501359c3433d5616f7078f99646d556bdda3c
nagamorikoshi <nagamori329@gmail.com>
2022-06-04 01:18:59 +0000

    publicコントローラーaddresses完了

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,2 +1,41 @@
 class Public::AddressesController < ApplicationController
+  before_action :authneticate_customer!
+  before_action :ensure_address, only: [:edit, :update, :destroy]
+
+  def index
+    @addresses = current_customer.addresses
+    @address = Addres.new
+  end
+
+  def create
+    @addresses = current_customer.addresses
+    @address = Address.new(address_params)
+    @address.customer_id = current_customer.id
+    @address.save ? (redirect_to addresses_path) : (render :index)
+  end
+
+  def edit
+  end
+
+  def update
+    @address.update(address_parmas) ? (redirect_to addresses_path) : (render :edit)
+  end
+
+  def destroy
+    @address.destroy
+    redirect_to addresses_path
+  end
+
+  private
+
+  def ensure_address
+    @addresses = current_customer.addresses
+    @address = @addresses.find_by(id: params[:id])
+    redirect_to addresses_path unless @address
+  end
+
+  def address_params
+    params.require(:address).permit(:potal_code, :destination, :name)
+  end
+
 end
