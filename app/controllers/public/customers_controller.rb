e02513f69a8df85a4327bb148a3b70309014ffe2
nagamorikoshi <nagamori329@gmail.com>
2022-06-05 01:07:17 +0000

    コントローラー完了

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,2 +1,38 @@
 class Public::CustomersController < ApplicationController
+  before_action :authenticate_customer!
+  before_action :set_current_customer
+  
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @customer.update(customer_params)
+      redirect_to mypage_path, notice: '会員情報の更新が完了しました。'
+    else
+      render :edit
+    end
+  end
+
+  def unsubscibr
+  end
+
+  def withdraw
+    @customer.update(is_active: false)
+    reset_session
+    redirect_to root_path
+  end
+
+  private
+  
+  def set_current_customer
+    @customer = current_customer
+  end
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :firts_name, :first_name_kana, :last_name_kana, :email, :postal_code, :address, :telephone_number)
+  end
+
 end
--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,2 +1,7 @@
 class Public::HomesController < ApplicationController
+  def top
+    @genres = Genre.only_active,includes(:items)
+    @items = Item.recommended
+  end
+
 end
